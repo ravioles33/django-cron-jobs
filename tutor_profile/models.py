a3590077ab256fce288293cf1c221f31400d4307
ravioles33 <juliann.mdq@gmail.com>
2024-10-28 12:13:41 -0300

    startapp tutor_profile; PRE DRF

---
+++ tutor_profile/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    lw_username = models.CharField(max_length=150, blank=True, null=True)
+    lw_password = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
